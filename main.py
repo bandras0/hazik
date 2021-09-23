c4afd5312763b9e37f2d1f5b214556410b04cd68
bandras0 <91279971+bandras0@users.noreply.github.com>
2021-09-24 00:03:36 +0200

    Add files via upload
    hf2_2

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+x1=float(input("Az elsö kordináta az x tengelyen"))
+x2=float(input("Az elsö kordináta az x tengelyen"))
+y1=float(input("Az elsö kordináta az y tengelyen"))
+y2=float(input("Az elsö kordináta az y tengelyen"))
+c1=(x2-x1)
+c2=(y2-y1)
+tavolsag=(math.sqrt((c1**2)+(c2**2)))
+print(tavolsag)
